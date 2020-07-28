31a54bc534b6c80ed32af9c396f14ad70899ab3f
Daniel Fernandez Rico <deferrico@gmail.com>
2020-07-28 18:45:03 +0200

    Added react select section on homepage

--- components/footer/component.js
+++ components/footer/component.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="c-footer wrapper">
       <span>Privacy policy</span>
       <span>Contact us</span>
       <span>Atlantic Council</span>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-import Main from 'components/main';
+import Select from 'react-select';
 
+import Main from 'components/main';
 import Button from 'components/button';
+
+const options = [
+  { value: 'OR', label: 'Oregon' },
+  { value: 'CA', label: 'California' },
+  { value: 'NV', label: 'Nevada' },
+];
 
 export default function Home() {
   return (
@@ -16,11 +23,39 @@ export default function Home() {
           </p>
           <Button
             className="home-cta"
-            colors={{ border: '#45bde1', text: '#FFF' }}
+            colors={{ border: '#4595e1', text: '#FFF' }}
             link="/explore"
           >
             Explore data
           </Button>
+        </div>
+        <div className="home-explore">
+          <img className="map-icon" src="assets/home-map.svg" alt="map-icon" />
+          <h2>
+            Explore the map, and select which state or county would you like to
+            analyze?
+          </h2>
+          <div className="home-search">
+            <Select
+              className="home-search--select"
+              options={options}
+              placeholder="Enter a state, county name or ZIP code"
+              styles={{
+                control: (provided) => ({ ...provided, height: '55px' }),
+                menu: (provided) => ({
+                  ...provided,
+                  color: '#000',
+                  textAlign: 'left',
+                }),
+              }}
+            />
+            <Button
+              className="search-btn"
+              colors={{ border: '#FFF', text: '#FFF' }}
+            >
+              Search all states
+            </Button>
+          </div>
         </div>
       </div>
     </Main>
