defe21cbb21ff599f2001258cfaa7afa9a550958
Daniel Fernandez Rico <deferrico@gmail.com>
2020-08-07 13:44:08 +0200

    Base homepage mobile styles

--- pages/index.js
+++ pages/index.js
@@ -5,58 +5,102 @@ import { connect } from 'react-redux';
 import Main from 'components/main';
 import Button from 'components/button';
 import Autocomplete from 'components/autocomplete';
+import { MediaContextProvider, Media } from 'components/media';
 
 import GeometriesProvider from 'providers/geometries';
 import { selectGeometriesProps } from 'providers/geometries/selectors';
 
+function HomeWrapper({ children }) {
+  HomeWrapper.propTypes = {
+    children: PropTypes.array,
+  };
+
+  return (
+    <MediaContextProvider>
+      <Media lessThan="small">
+        <Main>
+          <div className="c-home --mobile">{children}</div>
+        </Main>
+      </Media>
+      <Media greaterThanOrEqual="small">
+        <Main>
+          <div className="c-home">{children}</div>
+        </Main>
+      </Media>
+    </MediaContextProvider>
+  );
+}
+
 function Home({ locations }) {
   return (
-    <Main>
-      <div className="c-home">
-        <div className="home-greeting">
-          <div className="home-greeting--wrapper">
-            <h2>Welcome to</h2>
-            <h1>USA Resilience Map</h1>
-            <div className="break-line" />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-            <Button className="home-cta" link="/explore">
-              Explore data
-            </Button>
-          </div>
-          <div className="home-greeting--gradient" />
+    <HomeWrapper>
+      <div className="home-greeting">
+        <div className="home-greeting--wrapper">
+          <MediaContextProvider>
+            <Media greaterThanOrEqual="small">
+              <h2>Welcome to</h2>
+            </Media>
+          </MediaContextProvider>
+          <h1>USA Resilience Map</h1>
+          <div className="break-line" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <MediaContextProvider>
+            <Media greaterThanOrEqual="small">
+              <Button className="home-cta" link="/explore">
+                Explore data
+              </Button>
+            </Media>
+          </MediaContextProvider>
         </div>
-        <div className="home-explore">
-          <img
-            className="home-brush-stroke"
-            src="assets/images/home_brush_stroke.png"
-            alt="map-icon"
-          />
-          <img
-            className="map-icon"
-            src="assets/images/home-map.svg"
-            alt="map-icon"
-          />
-          <h2>
-            Explore the map, and select which state or county would you like to
-            analyze?
-          </h2>
-          <div className="home-search">
-            <Autocomplete
-              className="home-search--select"
-              clearable
-              options={locations}
+        <div className="home-greeting--gradient" />
+      </div>
+      <div className="home-explore">
+        <MediaContextProvider>
+          <Media lessThan="small">
+            <Main>
+              <img
+                className="map-icon"
+                src="assets/images/laptop_picto02.svg"
+                alt="map-icon"
+              />
+              <h2>
+                Explore and analyze any given county or state from your desktop.
+              </h2>
+            </Main>
+          </Media>
+          <Media greaterThanOrEqual="small">
+            <img
+              className="home-brush-stroke"
+              src="assets/images/home_brush_stroke.png"
+              alt="map-icon"
             />
-            <Button className="search-btn" link="/explore?id=0">
-              Search all states
-            </Button>
-          </div>
-        </div>
+            <img
+              className="map-icon"
+              src="assets/images/home-map.svg"
+              alt="map-icon"
+            />
+            <h2>
+              Explore the map, and select which state or county would you like
+              to analyze?
+            </h2>
+            <div className="home-search">
+              <Autocomplete
+                className="home-search--select"
+                clearable
+                options={locations}
+              />
+              <Button className="search-btn" link="/explore?id=0">
+                Search all states
+              </Button>
+            </div>
+          </Media>
+        </MediaContextProvider>
       </div>
       <GeometriesProvider />
-    </Main>
+    </HomeWrapper>
   );
 }
 
