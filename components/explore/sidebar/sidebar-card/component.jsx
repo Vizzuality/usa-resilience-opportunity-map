d68effd8b33f40407edb1a9d7f8f20028571e1f1
Daniel Fernandez Rico <deferrico@gmail.com>
2020-10-07 18:02:20 +0200

    Fixed accidental chart text showing

--- components/explore/sidebar/sidebar-card/component.jsx
+++ components/explore/sidebar/sidebar-card/component.jsx
@@ -108,7 +108,7 @@ export default function sidebarCard({
           </div>
         </div>
       </div>
-      {chartVisible && (
+      {chartVisible && chartData && (
         <div>
           {`${currentLocation.label} ${startCase(
             item.name
