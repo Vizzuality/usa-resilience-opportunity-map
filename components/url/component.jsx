f6ce11a7e6d67501ceb90b7bb4fc732a1b693e2e
Daniel Fernandez Rico <deferrico@gmail.com>
2020-10-14 11:11:34 +0200

    Diff query keys to know which ones to update

--- components/url/component.jsx
+++ components/url/component.jsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import qs from 'query-string';
 import useDeepCompareEffect from 'utils/deepCompare';
 import isEmpty from 'lodash.isempty';
+import { diff } from 'utils/diff';
 
 export default function URL({
   type = 'push',
@@ -16,6 +17,7 @@ export default function URL({
 }) {
   const router = useRouter();
   const isMounted = useRef(false);
+  const prevQuery = useRef({});
   const { query, pathname } = router;
 
   useDeepCompareEffect(() => {
@@ -33,13 +35,19 @@ export default function URL({
   useDeepCompareEffect(() => {
     if (isMounted.current && !isEmpty(query)) {
       const queryKeys = Object.keys(queryParams);
+      const changedKeys = Object.keys(diff(query, prevQuery.current));
+      console.log(changedKeys, query, prevQuery.current);
 
       queryKeys.forEach((k) => {
-        if (queryFunction) queryFunction({ key: k, value: query[k] || null });
+        if (queryFunction && changedKeys.includes(k)) {
+          queryFunction({ key: k, value: query[k] || null });
+        }
       });
     } else {
       isMounted.current = true;
     }
+
+    prevQuery.current = query;
   }, [query]);
 
   return null;
