3943ba2b7cd2d62f66418129c1c1f1e2665e536c
Pedro Pimenta <pedro@pimenta.co>
2021-08-12 18:23:47 +0200

    Add Stories layout and Flicking library

--- pages/stories/index.js
+++ pages/stories/index.js
@@ -1,5 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Flicking from '@egjs/react-flicking';
 
-const Stories = () => <h1>Stories</h1>;
+import StoriesMain from 'layouts/stories';
+
+import styles from './styles.module.scss';
+
+const STORIES = [
+  {
+    title: 'story1',
+  },
+  {
+    title: 'story2',
+  },
+];
+
+const Story = ({ title }) => {
+  return (
+    <div className={styles.Story}>
+      <div className={styles.StoryTitle}>{title}</div>
+    </div>
+  );
+};
+
+Story.propTypes = {
+  title: PropTypes.string,
+};
+
+const Stories = () => {
+  return (
+    <StoriesMain>
+      <Flicking
+        align="prev"
+        horizontal={false}
+        style={{
+          width: '100%',
+        }}
+      >
+        {STORIES.map((story) => (
+          <Story title={story.title} />
+        ))}
+      </Flicking>
+    </StoriesMain>
+  );
+};
 
 export default Stories;
